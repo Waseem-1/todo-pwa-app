65608adc0caf5467258289c1b0698441b074f393
Waseem <wahmed@uni-koblenz.de>
2022-10-04 11:52:27 +0200

    Added public path in vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    pwa: {
-        name: 'todo-mastery'
-    }
-}
+  pwa: {
+    name: "todo-mastery",
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/todo-pwa-app/" : "/",
+};

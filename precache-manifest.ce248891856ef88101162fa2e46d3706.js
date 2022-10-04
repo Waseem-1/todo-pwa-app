fe90590e7ee50b5c6d5d06b22595d1741c881262
Waseem <wahmed@uni-koblenz.de>
2022-10-04 11:54:13 +0200

    adding dist subtree

---
+++ precache-manifest.ce248891856ef88101162fa2e46d3706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88e84303db331ccd02b",
+    "url": "/todo-pwa-app/css/app.3849fcfe.css"
+  },
+  {
+    "revision": "353bc1ef12f07a3ebd98987bde32d2b1",
+    "url": "/todo-pwa-app/index.html"
+  },
+  {
+    "revision": "d88e84303db331ccd02b",
+    "url": "/todo-pwa-app/js/app.dd67405b.js"
+  },
+  {
+    "revision": "24a55e17fdf5120fb2ca",
+    "url": "/todo-pwa-app/js/chunk-vendors.f63ef583.js"
+  },
+  {
+    "revision": "bff7605209b9a403029b8e6722fe4cef",
+    "url": "/todo-pwa-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todo-pwa-app/robots.txt"
+  }
+]);
